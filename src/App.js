fd754d8a732437ec389dbd135b6dfbd2b90393ee
Saksham <2020UCP1801@mnit.ac.in>
2021-09-16 07:32:40 +0530

    shop component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import Shop from "./Components/Shop";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <div className="container my-3">
+        <Shop />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Shop.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div className="text-center">
+      <button className="btn btn-outline-danger mx-2">-</button>
+      Shop item from here
+      <button className="btn btn-outline-success mx-2">+</button>
+    </div>
+  );
+};
+
+export default Shop;
